0b988fecaff7de77f12c4ab50cc807e2d6411141
Shubham Saurav <shubhamsinha186@gmail.com>
2020-12-02 20:21:09 +0530

    Cleared prime or not program

--- c_programming/lab/prime_or_not.c
+++ c_programming/lab/prime_or_not.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int num;
     printf("Please enter a number: ");
     scanf("%d", &num);
-    
-    if (num <= 1) {
+
+    if (num <= 1)
+    {
         return 0;
     }
 
-    for (int i = 2; i < num; i++){
-        if (num % i == 0){
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
             printf("\nNot a Prime number: %d \n", num);
             return 0;
         }
